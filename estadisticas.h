f9be0d334765de256fb6450cd76d9d379d6a5c28
Suri Arias <249509373+suriariaspin-tech@users.noreply.github.com>
2026-06-16 09:41:00 -0500

    comentarios

--- estadisticas.h
+++ estadisticas.h
@@ -1,6 +1,8 @@
 #ifndef ESTADISTICAS_H
 #define ESTADISTICAS_H
 
+// Modulo 5: funciones para mostrar estadísticas.
+
 void mostrarEstadisticas(
     int cantPendientes,
     int cantEnProceso,
--- validaciones.h
+++ validaciones.h
@@ -1,6 +1,8 @@
 #ifndef VALIDACIONES_H
 #define VALIDACIONES_H
 
+// Modulo 6: funciones para validar entradas del usuario.
+
 void LimpiarBuffer();
 int TextoValido(char texto[]);
 void ValidarTexto(char texto[], int tamano);
