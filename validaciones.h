64147e9f9bb60004672f3904cee58d9a19be9f0d
Suri Arias <249509373+suriariaspin-tech@users.noreply.github.com>
2026-06-15 22:42:04 -0500

    Archivo validaciones.h

---
+++ validaciones.h
@@ -0,0 +1,11 @@
+#ifndef VALIDACIONES_H
+#define VALIDACIONES_H
+
+void LimpiarBuffer();
+int TextoValido(char texto[]);
+void ValidarTexto(char texto[], int tamano);
+void ValidarCodigo(int *codigo);
+void ValidarEntero(int *numero);
+void ValidarPrioridad(char prioridad[]);
+
+#endif
